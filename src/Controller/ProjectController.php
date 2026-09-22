af32842ff014779f42a057ca79b4ddf65c347de5
Deneboo <deneborbit@hotmail.com>
2026-09-22 23:01:48 +0200

    integrate project details template

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +16,19 @@ final class ProjectController extends AbstractController
         $projects = $projectRepository->findAll();
         return $this->render('project/index.html.twig', [
             'projects' => $projects,
+        ]);
+    }
+
+    #[Route('/project/{id}', name: 'app_project_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(?Project $project): Response
+    {
+        $users = $project?->getUsers();
+        $tasks = $project?->getTasks();
+
+        return $this->render('project/show.html.twig', [
+            'project' => $project,
+            'users' => $users,
+            'tasks' => $tasks,
         ]);
     }
 }
